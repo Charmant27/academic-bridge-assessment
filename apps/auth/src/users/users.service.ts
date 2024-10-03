b9241d492d0ab542b9fda82069bb26f5de00cfd3
Charmant27 <charmantogatera@gmail.com>
2024-10-03 23:50:46 +0200

    Added JWT module and configuration

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
 import * as bcrypt from 'bcryptjs'
@@ -8,10 +8,18 @@ export class UsersService {
     constructor(private readonly usersRepository: UsersRepository) {}
 
     async signup(createUserDto: CreateUserDto) {
-        return this.usersRepository.create({
-            ...createUserDto,
-            password: await bcrypt.hash(createUserDto.password, 10),
-            date_created: undefined
-        })
+        try {
+            const user = await this.usersRepository.create({
+                ...createUserDto,
+                password: await bcrypt.hash(createUserDto.password, 10),
+                date_created: undefined
+            })
+            return user
+        } catch (error) {
+            if(error.code===11000) {
+                throw new ConflictException(`user with ${createUserDto.email} already exists`)
+            }
+            throw new InternalServerErrorException('Something went wrong')
+        }
     }
 }
