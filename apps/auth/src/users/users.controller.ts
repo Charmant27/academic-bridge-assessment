c41c1220acb011daae207972c3238941f384d88f
Charmant27 <charmantogatera@gmail.com>
2024-10-17 00:47:08 +0200

    Added password reset feature

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -4,6 +4,8 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { currentUser } from '../current-user.decorator';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { UserDocument } from './models/users.schema';
+import { ResetPasswordDto } from './dto/reset-password.dto';
+import { ResetPasswordRequestDto } from './dto/reset-password-request.dto';
 
 @Controller('users')
 export class UsersController {
@@ -18,5 +20,15 @@ export class UsersController {
     @UseGuards(JwtAuthGuard)
     async getUser(@currentUser() user: UserDocument) {
         return user
+    }
+
+    @Post('/request-password-reset')
+    async requestPasswordReset(@Body() resetPasswordRequestDto: ResetPasswordRequestDto) {
+        return this.usersService.requestPasswordReset(resetPasswordRequestDto.email)
+    }
+
+    @Post('/reset-password')
+    async resetPassword(@Body() resetPasswordDto: ResetPasswordDto) {
+        return this.usersService.resetPassword(resetPasswordDto.token, resetPasswordDto.newPassword)
     }
 }
--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,15 +1,17 @@
-import { ConflictException, Inject, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Inject, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
 import * as bcrypt from 'bcryptjs'
 import { GetUserDto } from './dto/get-user.dto';
 import { NOTIFICATIONS_SERVICE } from '@app/common/constants';
 import { ClientProxy } from '@nestjs/microservices';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
     constructor(
         private readonly usersRepository: UsersRepository,
+        private readonly jwtService: JwtService,
         @Inject(NOTIFICATIONS_SERVICE)
         private readonly notificationsService: ClientProxy
     ) {}
@@ -52,5 +54,51 @@ export class UsersService {
 
     async getUser(getUserDto: GetUserDto) {
         return this.usersRepository.findOne(getUserDto)
+    }
+
+    // request password reset
+    async requestPasswordReset(email: string) {
+        try {
+            const user = await this.usersRepository.findOne({email})
+
+            if (!user) {
+                throw new NotFoundException('User with this email does not exist')
+            }
+
+            const token = this.jwtService.sign({email}, {expiresIn: '1h'})
+
+            await this.notificationsService.emit('notify_email', {
+                to: email,
+                subject: 'Reset password',
+                text: `Use this code to reset your password: ${token}`,
+                html: `<p>Use the following code to reset your password <br> <strong>${token}</strong></p>`
+            })
+
+        } catch (error) {
+            console.log(`an error occurred: ${error}`)
+            throw new InternalServerErrorException('Failed to generate password reset code. Something went wrong')
+        }
+    }
+
+    // reset password 
+    async resetPassword(token: string, newPassword: string) {
+        try {
+            const {email} = this.jwtService.verify(token)
+            const user = await this.usersRepository.findOne({email})
+
+            if (!user) {
+                throw new NotFoundException('Invalid user email. please check your email')
+            }
+
+            const hashedPassword = await bcrypt.hash(newPassword, 10)
+
+            await this.usersRepository.findOneAndUpdate(user._id, {password: hashedPassword})
+
+            return { message: 'Password reset successfully.' };
+
+        } catch (error) {
+            console.log(`an error occurred: ${error}`)
+            throw new InternalServerErrorException('Failed to reset password')
+        }
     }
 }
